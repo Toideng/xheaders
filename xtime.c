21a8c75c8793c7e49859dadf9c14dc01ceec8f1d
Toideng <toideng@toideng.com>
2020-06-07 19:11:57 +0000

    Rework xtime API

--- xtime.c
+++ xtime.c
@@ -1,5 +1,6 @@
 #include <time.h>
 #include <unistd.h>
+#include "xtypes.h"
 #include "xtime.h"
 
 
@@ -11,74 +12,160 @@
 
 
 
-struct timestamp
-timestamp_get()
+utsa_t
+utsa_createnew()
 {
-	struct timestamp result;
-	struct timespec ts;
-	clock_gettime(CLOCK_REALTIME, &ts);
+	utsa_t result;
 
-	result.secs = (i64)ts.tv_sec - 946684800LL; // seconds since 2000-01-01
-	result.frac = (u64)ts.tv_nsec;
+	struct timespec ts_high; // high-precision nonsence clock
+	struct timespec ts_low; // low-precision adjusted clock
+	clock_gettime(CLOCK_MONOTONIC_RAW, &ts_high);
+	clock_gettime(CLOCK_REALTIME, &ts_low);
+
+	result.stamp.secs = (i64)ts_low.tv_sec - NEWEPOCH;
+	result.stamp.frac = (u64)ts_low.tv_nsec;
+	result.stamp.frac <<= 32;
+	result.stamp.frac /= 1000000000;
+	result.stamp.frac <<= 32; // nanoseconds to 64-pow fracs
+
+	result.adjust.secs = (i64)ts_high.tv_sec;
+	result.adjust.frac = (u64)ts_high.tv_nsec;
+	result.adjust.frac <<= 32;
+	result.adjust.frac /= 1000000000;
+	result.adjust.frac <<= 32; // nanoseconds to 64-pow fracs
+
+	uts_sub(&result.adjust, &result.stamp, &result.adjust);
+	return result;
+}
+
+
+
+utsa_t *
+utsa_get(
+	utsa_t *astamp
+	)
+{
+	struct timespec ts;
+	clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+
+	astamp.stamp.secs = (i64)ts.tv_sec;
+	astamp.stamp.frac = (u64)ts.tv_nsec;
+	astamp.stamp.frac <<= 32;
+	astamp.stamp.frac /= 1000000000;
+	astamp.stamp.frac <<= 32;
+
+	uts_add(astamp.stamp, astamp.stamp, astamp.adjust);
+	return astamp;
+}
+
+
+
+struct tm *
+uts_gmtime_r(
+	uts_t *stamp,
+	struct tm *result
+	)
+{
+	u64 usecs = uts->frac;
+	usecs >>= 32;
+	usecs *= 1000000000;
+	usecs >>= 32;
+
+	result->tm_isdst = (int)usecs;
+	result->tm_sec = (int)(stamp->secs % 60);
+
+	// ...
+	// TODO
+
+	return result;
+}
+
+
+
+uts_t
+uts_mktime(
+	struct tm *tm
+	)
+{
+	uts_t result;
+
+	result.frac = (u64)tm.tm_isdst;
 	result.frac <<= 32;
 	result.frac /= 1000000000;
 	result.frac <<= 32; // nanoseconds to 64-pow fracs
 
+	result.secs = tm->tm_sec;
+
+	// ...
+	// TODO
+
 	return result;
 }
 
 
 
-
-
-
-
-
-
-
-// Returns a - b
-struct timestamp
-timestamp_diff(
-	struct timestamp a,
-	struct timestamp b)
+uts_t *
+uts_sub(
+	uts_t *result, // = a - b
+	uts_t *a,
+	uts_t *b
+	)
 {
-	struct timestamp result;
-
-	if (a.frac >= b.frac)
-	{
-		result.secs = a.secs - b.secs;
-		result.frac = a.frac - b.frac;
-	}
+	if (a->frac >= b->frac)
+		result->secs = a->secs - b->secs;
 	else
-	{
-		result.secs = a.secs - b.secs - 1;
-		result.frac = a.frac - b.frac;
-	}
+		result->secs = a->secs - b->secs - 1;
+	result->frac = a->frac - b->frac;
 
 	return result;
 }
 
 
 
-u32
-timestamp_to_nsecs(
-	struct timestamp stamp)
+
+uts_t *
+uts_add(
+	uts_t *result, // = a + b
+	uts_t *a,
+	uts_t *b
+	)
 {
-	u64 result = stamp.frac;
-	result >>= 32;
-	result *= 1000000000;
-	result >>= 32;
-	return ((u32)result);
+	u64 fracsum = a->frac + b->frac;
+
+	if (a->frac <= fracsum)
+		result->secs = a->secs + b->secs;
+	else
+		result->secs = a->secs + b->secs + 1;
+	result->frac = fracsum;
+
+	return result;
 }
+
 
 
 void
-sleep_timestamp(
-	struct timestamp stamp)
+uts_sleep(
+	uts_t *uts
+	)
 {
-	if (stamp.secs < 0)
+	u64 usecs = uts->frac;
+	usecs >>= 32;
+	usecs *= 1000000;
+	usecs >>= 32;
+
+	if (uts->secs < 0)
 		return;
-	sleep(stamp.secs);
-	stamp.secs = 0;
-	usleep(timestamp_to_nsecs(stamp)/1000);
+
+	sleep(uts->secs);
+	usleep(usecs);
 }
+
+
+
+
+
+
+
+
+
+
--- xtime.h
+++ xtime.h
@@ -1,39 +1,99 @@
 #ifndef XTIME_H
 #define XTIME_H
 
+#include <time.h>
 #include "xtypes.h"
 
 
-struct timestamp
+typedef union
 {
-	i64 secs; // Seconds since 2000-01-01
-	u64 frac; // Fractions of a second since last integral second
-};
+	// Technically it is a 128-bit integer number in two's complement
+	// interpreted as 128.64 fixed-point fraction
+	struct
+	{
+		u64 frac; // Fractions of a second since last integer second
+		i64 secs; // Seconds since 2000-01-01
+	};
+	byte data[16];
+} uts_t; // "Universal TimeStamp"
+
+typedef struct
+{
+	/* A real timestamp corresponding to an actual point in time */
+	uts_t stamp;
+	/* `adjust` is added (sign-awarily) to the raw hardware clock to get the stamp */
+	uts_t adjust;
+} utsa_t; // "Universal TimeStamp with Adjustment"
 
 
 
-struct timestamp
-timestamp_get();
-
-
-
-// Returns a - b
-struct timestamp
-timestamp_diff(
-	struct timestamp a,
-	struct timestamp b
+utsa_t
+utsa_createnew(
 );
 
-u32
-timestamp_to_nsecs(
-	struct timestamp stamp
+utsa_t *
+utsa_get(
+	utsa_t *astamp
+);
+
+/*
+
+How `struct tm` is used here:
+
+```
+struct tm {
+	int tm_sec;    // Seconds (0-59)
+	int tm_min;    // Minutes (0-59)
+	int tm_hour;   // Hours (0-23)
+	int tm_mday;   // Day of the month (0-30)
+	int tm_mon;    // Month (0-11)
+	int tm_year;   // Year - 2000
+	int tm_wday;   // (ignored on input) Day of the week (0-6, Monday = 0)
+	int tm_yday;   // Day in the year (0-365, 1 Jan = 0)
+	int tm_isdst;  // Nanoseconds (it's the only place to put them)
+};
+```
+
+Notice change from leap seconds, mondays, origin at 2000, DoM from 0 and
+nanoseconds
+
+*/
+
+struct tm *
+uts_gmtime_r(
+	uts_t *stamp,
+	struct tm *result
+);
+
+uts_t
+uts_mktime(
+	struct tm *tm
+);
+
+
+
+// Both uts_sub and uts_add guarantee correct behaviour if result coincides with
+// either a or b or both
+
+uts_t *
+uts_sub(
+	uts_t *result, // = a - b
+	uts_t *a,
+	uts_t *b
+);
+
+uts_t *
+uts_add(
+	uts_t *result, // = a + b
+	uts_t *a,
+	uts_t *b
 );
 
 
 
 void
-sleep_timestamp(
-	struct timestamp stamp
+uts_sleep(
+	uts_t *uts
 );
 
 
